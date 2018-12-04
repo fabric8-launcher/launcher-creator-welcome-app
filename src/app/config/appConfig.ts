fabric8-launcher/launcher-creator-welcome-app

commit 35e31352a0a000e005fdb4eee522b78d1b6c1837

fix: make console url optional

src/app/config/appConfig.ts
@@ -22,9 +22,10 @@ if (!isMockMode) {
   } catch (e) {
     throw new Error('Error while parsing WelcomeApp config: ' + e.toString());
   }
-  appConfig.consoleUrl = checkNotNull(process.env.REACT_APP_OPENSHIFT_CONSOLE_URL, 'process.env.REACT_APP_OPENSHIFT_CONSOLE_URL');
+  const consoleUrl = rocess.env.REACT_APP_OPENSHIFT_CONSOLE || '';
   const repositoryUrl = process.env.REACT_APP_SOURCE_REPOSITORY_URL || '';
   appConfig.sourceRepositoryUrl = repositoryUrl.length > 0 ? repositoryUrl : undefined;
+  appConfig.consoleUrl = consoleUrl.length > 0 ? consoleUrl : undefined;
 } else {
   appConfig.definition = mockAppDefinition;
   appConfig.consoleUrl = 'http://consoleUrl.mock.io';

commit 214fff293d013ff0b9473eca6af6681c463afb46

chore: fixed compilation error introduced by 35e31352a0a000e005fdb4eee522b78d1b6c1837

src/app/config/appConfig.ts
@@ -22,7 +22,7 @@ if (!isMockMode) {
   } catch (e) {
     throw new Error('Error while parsing WelcomeApp config: ' + e.toString());
   }
-  const consoleUrl = rocess.env.REACT_APP_OPENSHIFT_CONSOLE || '';
+  const consoleUrl = process.env.REACT_APP_OPENSHIFT_CONSOLE || '';
   const repositoryUrl = process.env.REACT_APP_SOURCE_REPOSITORY_URL || '';
   appConfig.sourceRepositoryUrl = repositoryUrl.length > 0 ? repositoryUrl : undefined;
   appConfig.consoleUrl = consoleUrl.length > 0 ? consoleUrl : undefined;