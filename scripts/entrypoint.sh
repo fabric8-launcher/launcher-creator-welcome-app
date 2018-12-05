71f2a3fe3ba4ffede6cdfd6ed07a3784570c04e4
George Gastaldi <gegastaldi@gmail.com>
2018-12-05 14:39:12 -0200

    fix: escape backslashes. Fixes #60

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 CONFIG_TEMPLATE="/usr/bin/config-tmpl.json"
 INDEX="/usr/share/nginx/html/index.html"
 
-export WELCOME_APP_CONFIG_ENCODED=$(echo ${WELCOME_APP_CONFIG} | sed 's/\"/\\\"/g' | sed s/\'/\\\'/g)
+export WELCOME_APP_CONFIG_ENCODED=$(echo ${WELCOME_APP_CONFIG} | sed 's/\\/\\\\/g' | sed 's/\"/\\\"/g' | sed s/\'/\\\'/g)
 
 # Encode quotes and remove line breaks
 ENCODED_CONFIG=$(envsubst < ${CONFIG_TEMPLATE} | sed -e 's/[]\/$*.^[]/\\&/g' | sed ':a;N;$!ba;s/\n/ /g')
