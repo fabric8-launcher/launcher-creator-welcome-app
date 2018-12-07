4a9f0e549e2a78c3aa9f4d4ced230f2312726588
ia3andy <adamevin@redhat.com>
2018-12-07 15:59:32 +0100

    fix: console placeholder

--- src/shared/components/Console.tsx
+++ src/shared/components/Console.tsx
@@ -4,13 +4,25 @@ import { Component } from 'react';
 import './Console.css';
 
 interface ConsoleProps {
-   id: string;
-   content?: Array<(string | React.ReactNode)> };
+  id: string;
+  content?: Array<(string | React.ReactNode)>;
+}
+
+interface ConsoleState {
+  styleClass: string;
+}
 
 
-export class Console extends Component<ConsoleProps> {
+export class Console extends Component<ConsoleProps, ConsoleState> {
 
   private readonly ref = React.createRef<HTMLDivElement>();
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      styleClass: 'no-content',
+    }
+  }
 
   public componentDidUpdate() {
     if (this.ref.current) {
@@ -18,15 +30,26 @@ export class Console extends Component<ConsoleProps> {
         const offset = (this.ref.current.lastChild as HTMLDivElement).offsetTop - this.ref.current.offsetTop;
         this.ref.current.scrollTop = offset;
       }
-      this.ref.current.style.height = this.ref.current.scrollHeight <= 130 ? '130px' : '300px';
+      const contentSize = this.props.content ? this.props.content.length : 0; 
+
+      if(contentSize === 0 && this.state.styleClass !== 'no-content') {
+        this.setState({ styleClass: 'no-content' });
+      }
+      if(contentSize === 1 && this.state.styleClass !== 'initial') {
+        this.setState({ styleClass: 'initial' });
+      }
+      if(contentSize > 1 && this.state.styleClass !== 'large') {
+        this.setState({ styleClass: 'large' });
+      }
     }
   }
 
   public render() {
     const consoleContent = this.props.content || [];
     return (
-      <div className="console" id={this.props.id}>
-        <div className="console-content" ref={this.ref}>
+      <div className={`console`} id={this.props.id}>
+        <div className={`console-content ${this.state.styleClass}`} ref={this.ref}>
+          <span className="placeholder">>> Execute some actions to get things started in this console...</span>
           {consoleContent.map((c, i) => (<div key={i} className="console-content-row">{c}</div>))}
         </div>
       </div>
