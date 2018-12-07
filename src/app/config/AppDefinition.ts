7e3d6b058c3382403ad929fe082d52304c4952a5
ia3andy <adamevin@redhat.com>
2018-12-07 13:40:32 +0100

    fix: add tier support

--- src/app/config/AppDefinition.ts
+++ src/app/config/AppDefinition.ts
@@ -1,28 +1,36 @@
 export default interface AppDefinition {
-  application: string,
-  shared: {
-    runtime: string,
-    groupId?: string,
-    artifactId?: string,
-    version?: string,
-  },
-  extra: {
-    runtimeImage: string;
-    runtimeInfo: {
-      id: string;
-      name: string;
-      description: string;
-      icon: string;
-      metadata: {
-        language: string;
-      }
+  application: string;
+  tiers: Array<{
+    tier: string;
+    shared: {
+      runtime: {
+        name: string;
+      },
+      maven: {
+        groupId?: string;
+        artifactId?: string;
+        version?: string;
+      },
+    },
+    extra: {
+      runtimeImage: string;
+      runtimeInfo: {
+        id: string;
+        name: string;
+        description: string;
+        icon: string;
+        metadata: {
+          language: string;
+        }
+      };
+      runtimeService: string;
+      runtimeRoute: string;
     };
-    runtimeService: string;
-  };
-  capabilities: Array<{
-    module: string,
-    props: { [propId: string]: string; },
-    extra: { [propId: string]: string; },
-  }>;
+    capabilities: Array<{
+      module: string,
+      props: { [propId: string]: string; },
+      extra: { [propId: string]: string; },
+    }>;
+  }>
 }
   
