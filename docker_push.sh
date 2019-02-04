44a685a34f3cd4ef46fc33b12363316596687236
George Gastaldi <gegastaldi@gmail.com>
2019-02-04 12:56:36 -0200

    chore: improved getting commit revision

--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+REVISION=$(echo $(git show --format=%H -s HEAD) | cut -c1-7)
+
 docker build -t fabric8/launcher-creator-welcome-app:latest . 
-docker tag fabric8/launcher-creator-welcome-app:latest fabric8/launcher-creator-welcome-app:$(echo $REVISION | cut -c1-7) 
+docker tag fabric8/launcher-creator-welcome-app:latest fabric8/launcher-creator-welcome-app:$REVISION
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker push fabric8/launcher-creator-welcome-app:latest 
-docker push fabric8/launcher-creator-welcome-app:$(echo $REVISION | cut -c1-7) 
+docker push fabric8/launcher-creator-welcome-app:$REVISION
