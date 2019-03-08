850608a9281d84fa75ff9bf3c45342e6cb4ca526
George Gastaldi <gegastaldi@gmail.com>
2019-03-08 11:40:59 -0300

    chore: change how docker login is performed

--- docker_push.sh
+++ docker_push.sh
@@ -4,6 +4,6 @@ REVISION=$(echo $(git show --format=%H -s HEAD) | cut -c1-7)
 
 docker build -t fabric8/launcher-creator-welcome-app:latest . 
 docker tag fabric8/launcher-creator-welcome-app:latest fabric8/launcher-creator-welcome-app:$REVISION
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker push fabric8/launcher-creator-welcome-app:latest 
 docker push fabric8/launcher-creator-welcome-app:$REVISION
